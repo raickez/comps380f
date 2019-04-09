9a3801a510717398c2cbe5986e2490a5c9d620f1
User <User@192.168.238.88>
2019-04-09 14:49:38 +0800

    sql added

--- src/main/resources/create_comments.sql
+++ src/main/resources/create_comments.sql
@@ -1,8 +1,8 @@
 CREATE TABLE comment (
-    user_role_id INTEGER NOT NULL,
+    username VARCHAR(50) NOT NULL,
     course_id INTEGER NOT NULL, 
     comment VARCHAR(255) NOT NULL,
-    PRIMARY KEY (user_role_id,course_id),
-    FOREIGN KEY (user_role_id) REFERENCES user_roles(user_role_id), 
+    PRIMARY KEY (username,course_id),
+    FOREIGN KEY (username) REFERENCES users(username), 
     FOREIGN KEY (course_id) REFERENCES course(id) 
 );
